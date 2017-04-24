fc24afb8b159732b48c9b97e452de9cec2b14890
ejmejm <ninjaedan@aol.com>
2017-04-23 22:50:01 -0500

    work

--- nn.py
+++ nn.py
@@ -13,20 +13,27 @@ nIn = 1
 nHidden = 10
 nOut = 1
 alpha = 0.01
+sampleSize = 1;
 
 def train(X, Y, W1, W2):
     #Forward propogation
-    l1 = X * W1
-    l2 = sigmoid(l1) * W2
+    l1 = np.dot(X, W1)
+    print("l1: ", l1.shape)
+    l2 = np.dot(sigmoid(l1), W2).reshape()
+    print("l2: ", l2.shape)
 
     #Back propogation & loss calculation
-    loss2 = sigmoidPrime(l2) * (Y - sigmoid(l2))**2
-    loss1 = sigmoidPrime(l1) * (loss2 * W2)**2
+    loss2 = sigmoidPrime(l2) * (Y - sigmoid(l2)) #try without sigmoid
+    print("loss2: ", loss2.shape)
+    loss1 = sigmoidPrime(l1) * (loss2 * W2).transpose()
+    print("loss1: ", loss1.shape)
     #print("Loss: ", np.sum(loss2))
 
     #Update weights
-    W1 = W1 + alpha * (X * loss1)
-    W2 = W2 + alpha * (l1 * loss2)
+    print("W1: ", np.dot(sigmoid(X), loss1).shape)
+    W1 = W1 + alpha * np.dot(sigmoid(X), loss1)
+    print("W2: ", np.dot(sigmoid(l1), loss2).shape)
+    W2 = W2 + alpha * np.dot(sigmoid(l1), loss2)
 
     return W1, W2, np.abs(loss2).sum()
 
@@ -34,26 +41,28 @@ def predict(X, Y, W1, W2):
     #Forward propogation
     l1 = X * W1
     l2 = sigmoid(l1) * W2
-    print(l2.size)
+    #print(W1.size)
     if Y == l2.sum():
         return 1
     else:
         return 0
 
-W1 = np.random.normal(scale=0.1, size=(nIn, nHidden))
-W2 = np.random.normal(scale=0.1, size=(nHidden, nOut))
+W1 = np.random.rand(nIn, nHidden) * 0.5
+print(W1.shape)
+W2 = np.random.rand(nHidden, nOut) * 0.5
+print(W2.shape)
 
-for i in range(10000):
+for i in range(sampleSize):
     z = np.random.choice(2, 1)
     W1, W2, loss = train(z, 1-z, W1, W2)
     if i % 1000 == 0:
         print(loss)
 
 count = 0
-for i in range(10000):
+for i in range(sampleSize):
     z = np.random.choice(2, 1)
     if predict(z, 1-z, W1, W2) == 1:
-        print("Predict: ", predict(z, 1-z, W1, W2))
+        #print("Predict: ", predict(z, 1-z, W1, W2))
         count += 1;
 
 print("Accuracy: ", count/100000)
