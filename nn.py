8ff1d5b8ea38f622f3355f964f42abb3a904ade9
ejmejm <ninjaedan@aol.com>
2017-04-23 23:09:56 -0500

    Still broken

--- nn.py
+++ nn.py
@@ -13,13 +13,13 @@ nIn = 1
 nHidden = 10
 nOut = 1
 alpha = 0.01
-sampleSize = 1;
+sampleSize = 2;
 
 def train(X, Y, W1, W2):
     #Forward propogation
-    l1 = np.dot(X, W1)
+    l1 = np.asmatrix(np.dot(X, W1))
     print("l1: ", l1.shape)
-    l2 = np.dot(sigmoid(l1), W2).reshape()
+    l2 = np.asmatrix(np.dot(sigmoid(l1), W2)
     print("l2: ", l2.shape)
 
     #Back propogation & loss calculation
@@ -30,10 +30,10 @@ def train(X, Y, W1, W2):
     #print("Loss: ", np.sum(loss2))
 
     #Update weights
-    print("W1: ", np.dot(sigmoid(X), loss1).shape)
-    W1 = W1 + alpha * np.dot(sigmoid(X), loss1)
-    print("W2: ", np.dot(sigmoid(l1), loss2).shape)
-    W2 = W2 + alpha * np.dot(sigmoid(l1), loss2)
+    print("W1: ", np.asmatrix(np.dot(sigmoid(X), loss1)).shape)
+    W1 = W1 + alpha * np.asmatrix(np.dot(sigmoid(X), loss1))
+    print("W2: ", np.dot(np.asmatrix(sigmoid(l1)).transpose(), np.asmatrix(loss2)).shape)
+    W2 = W2 + alpha * np.dot(np.asmatrix(sigmoid(l1)).transpose(), np.asmatrix(loss2))
 
     return W1, W2, np.abs(loss2).sum()
 
@@ -48,9 +48,9 @@ def predict(X, Y, W1, W2):
         return 0
 
 W1 = np.random.rand(nIn, nHidden) * 0.5
-print(W1.shape)
+print("W1 (Original): ", W1.shape)
 W2 = np.random.rand(nHidden, nOut) * 0.5
-print(W2.shape)
+print("W2 (Original): ", W2.shape)
 
 for i in range(sampleSize):
     z = np.random.choice(2, 1)
