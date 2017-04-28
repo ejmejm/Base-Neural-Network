c9d7f21e2413b90333e56447d159726ad9e030f6
ejmejm <ninjaedan@aol.com>
2017-04-27 20:38:26 -0400

    Done testing

--- src/nn.py
+++ src/nn.py
@@ -1,6 +1,4 @@
 import numpy as np
-from matplotlib import pyplot as plt
-from sklearn import datasets
 
 np.random.seed(0)
 
@@ -10,20 +8,15 @@ def sigmoid(x):
 def sigmoidPrime(x):
     return sigmoid(x) * (1 - sigmoid(x))
 
-iris = datasets.load_iris()
-irisX = np.asarray(iris.data[:, :]).reshape(150, 4)  # we only take the first two features.
-irisY = np.asarray(iris.target).reshape(150, 1)
-
-permutation = np.random.permutation(irisX.shape[0])
-
-irisX = irisX[permutation]
-irisY = irisY[permutation]
+data = np.load("src/irisData.npz")
+irisX = data["irisX"]
+irisY = data["irisY"]
 
 nIn = 4
 nHidden = 5
 nOut = 1
-alpha = 0.1
-sampleSize = 2000;
+alpha = 1.0
+sampleSize = 500;
 
 def train(X, Y, W1, W2):
     #Forward propogation
@@ -41,10 +34,8 @@ def train(X, Y, W1, W2):
 
     W2 = W2 - alpha * deltaW2
     W1 = W1 - alpha * deltaW1
-    #W2 = W2 - alpha * al3 * (al3 - Y) * al3 * (1 - al3)
-    #W1 = W1 - alpha * al2 * np.dot((al3 - Y) * al3 * (1 - al3), W2.transpose()) * al2 * (1 - al2)
 
-    return W1, W2, loss
+    return W1, W2, np.sum((Y-al3)**2)/nIn * 100
 
 def predict(X, W1, W2):
     #Forward propogation
@@ -52,14 +43,16 @@ def predict(X, W1, W2):
     l3 = np.dot(sigmoid(l2), W2)
     return sigmoid(l3)
 
+######################################################## Function Calls Start
+
 W1 = np.random.rand(nIn, nHidden) * 0.5
 W2 = np.random.rand(nHidden, nOut) * 0.5
+
 for j in range(sampleSize):
     for i in range(irisX.shape[0] - 15):
         W1, W2, loss = train(irisX[i].reshape(1, 4), irisY[i].reshape(1, 1)/2, W1, W2)
-    if j % 200 == 0:
+    if j % 50 == 0:
         print("Loss: ", loss)
 
 for i in range(15):
-    print("P: ", predict(irisX[i+135], W1, W2)*2, " -> ", round(np.asscalar(predict(irisX[i+135], W1, W2)*2)))
-    print("A: ", irisY[i+135])
+    print("P: ", predict(irisX[i+135], W1, W2)*2, " -> ", round(np.asscalar(predict(irisX[i+135], W1, W2)*2)), " : ", np.asscalar(irisY[i+135]))
